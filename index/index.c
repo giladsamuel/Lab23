1749407a550cbc6645cf41100c27420b7b660b7e
GILAD SAMUEL <giladsam98@gmail.com>
2024-01-22 21:23:50 +0200

    copilot prog working

--- index/index.c
+++ index/index.c
@@ -21,22 +21,25 @@ int main() {
     char line[100];
     WordIndex wordIndex[MAX_WORDS];
     int wordCount = 0;
+    FILE *file;
+    int lineNumber;
+    int i;
+    int j;
 
     printf("Enter the name of the file to index: ");
     scanf("%s", filename);
     printf("You entered: %s\n", filename);
 
-    FILE *file = fopen(filename, "r");
+    file = fopen(filename, "r");
     if (file == NULL) {
         printf("Failed to open the file.\n");
         return 1;
     }
 
-    int lineNumber = 1;
+    lineNumber = 1;
     while (fgets(line, sizeof(line), file) != NULL) {
         char *word = strtok(line, " \t\n");
         while (word != NULL) {
-            int i;
             for (i = 0; i < wordCount; i++) {
                 if (strcmp(wordIndex[i].word, word) == 0) {
                     wordIndex[i].lineNumbers[wordIndex[i].count++] = lineNumber;
@@ -57,9 +60,9 @@ int main() {
 
     qsort(wordIndex, wordCount, sizeof(WordIndex), compareWords);
 
-    for (int i = 0; i < wordCount; i++) {
+    for (i = 0; i < wordCount; i++) {
         printf("%s: ", wordIndex[i].word);
-        for (int j = 0; j < wordIndex[i].count; j++) {
+        for (j = 0; j < wordIndex[i].count; j++) {
             printf("%d ", wordIndex[i].lineNumbers[j]);
         }
         printf("\n");
