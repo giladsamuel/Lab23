60ebc38e6ca937b3d4873a8f6707e48bf5c11859
GILAD SAMUEL <giladsam98@gmail.com>
2024-01-22 14:45:40 +0200

    Initial commit

---
+++ index/index.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_WORDS 1000
+
+typedef struct {
+    char word[100];
+    int lineNumbers[MAX_WORDS];
+    int count;
+} WordIndex;
+
+int compareWords(const void *a, const void *b) {
+    const WordIndex *wordIndexA = (const WordIndex *)a;
+    const WordIndex *wordIndexB = (const WordIndex *)b;
+    return strcmp(wordIndexA->word, wordIndexB->word);
+}
+
+int main() {
+    char filename[100];
+    char line[100];
+    WordIndex wordIndex[MAX_WORDS];
+    int wordCount = 0;
+
+    printf("Enter the name of the file to index: ");
+    scanf("%s", filename);
+    printf("You entered: %s\n", filename);
+
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Failed to open the file.\n");
+        return 1;
+    }
+
+    int lineNumber = 1;
+    while (fgets(line, sizeof(line), file) != NULL) {
+        char *word = strtok(line, " \t\n");
+        while (word != NULL) {
+            int i;
+            for (i = 0; i < wordCount; i++) {
+                if (strcmp(wordIndex[i].word, word) == 0) {
+                    wordIndex[i].lineNumbers[wordIndex[i].count++] = lineNumber;
+                    break;
+                }
+            }
+            if (i == wordCount) {
+                strcpy(wordIndex[wordCount].word, word);
+                wordIndex[wordCount].lineNumbers[wordIndex[wordCount].count++] = lineNumber;
+                wordCount++;
+            }
+            word = strtok(NULL, " \t\n");
+        }
+        lineNumber++;
+    }
+
+    fclose(file);
+
+    qsort(wordIndex, wordCount, sizeof(WordIndex), compareWords);
+
+    for (int i = 0; i < wordCount; i++) {
+        printf("%s: ", wordIndex[i].word);
+        for (int j = 0; j < wordIndex[i].count; j++) {
+            printf("%d ", wordIndex[i].lineNumbers[j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
